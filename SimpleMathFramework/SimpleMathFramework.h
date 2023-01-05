ecde330749ef14b626beed65a3e97a3e1a994320
Antonio Hernandez <jose.hernandezam@elektra.com.mx>
2023-01-05 12:15:49 -0600

    Initial Commit

---
+++ SimpleMathFramework/SimpleMathFramework.h
@@ -0,0 +1,18 @@
+//
+//  SimpleMathFramework.h
+//  SimpleMathFramework
+//
+//  Created by Jose Antonio Hernandez on 05/01/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SimpleMathFramework.
+FOUNDATION_EXPORT double SimpleMathFrameworkVersionNumber;
+
+//! Project version string for SimpleMathFramework.
+FOUNDATION_EXPORT const unsigned char SimpleMathFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SimpleMathFramework/PublicHeader.h>
+
+
