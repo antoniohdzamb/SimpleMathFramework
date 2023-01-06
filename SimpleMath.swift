2c7a55a8613188927cb1f8453307f905534e69d6
JOSE ANTONIO HERNANDEZ AMBROCIO <jose.hernandezam@elektra.com.mx>
2023-01-06 14:10:18 -0600

    static functions and zip

--- SimpleMath.swift
+++ SimpleMath.swift
@@ -9,7 +9,7 @@ import Foundation
 
 public class SimpleAlgorithms {
 
-    public func fibonacci(_ n: Int) -> Int {
+    public static func fibonacci(_ n: Int) -> Int {
         var a = 1
         var b = 1
         guard n > 1 else { return a }
@@ -20,7 +20,7 @@ public class SimpleAlgorithms {
         return a
     }
 
-    public func factorial(n: Int) -> Int {
+    public static func factorial(n: Int) -> Int {
         var result = 1
         if n > 0 {
             (1...n).forEach { i in
